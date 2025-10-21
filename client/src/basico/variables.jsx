2fce58c2362789581987cba2db526db0ff192d12
Adrian Rubio <adrianrh13743@gmail.com>
2025-10-21 13:34:14 +0200

    v1.0-variables

--- client/src/basico/variables.jsx
+++ client/src/basico/variables.jsx
@@ -6,14 +6,14 @@ const objeto = {nombre:'Variables en JSX'} // EN LOS OBJETOS REACT NO SABE COMO 
 const fecha = new Date();// EN LOS OBJETOS REACT NO SABE COMO COVERTIRLOS
 export const Variables = () => {
     return(
-        <div>
+        <>
             <p>{texto}</p>
             <p>{numeros}</p>
             <p>{lista}</p>
             <p>{funcion()}</p>
             <p>{JSON.stringify(objeto)}</p> {/* Convierte el objeto en una cadena de texto formato JSON.*/}
             <p>{JSON.stringify(fecha)}</p> {/* Convierte el objeto en una cadena de texto formato JSON.*/}   
-        </div>
+        </>
     ) // solo puede devolver un objeto, por eso se agrupa en el div
 }
 

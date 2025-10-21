90a6f5119d7ae7b378cf633c07db5f8b4754b0c5
Adrian Rubio <adrianrh13743@gmail.com>
2025-10-21 13:05:22 +0200

    v1.0

---
+++ client/src/basico/componenteBasico.jsx
@@ -0,0 +1,19 @@
+const texto = 'texto formato string';
+const numeros = 12345;
+const lista = ['Adrian Rubio', 1998];
+const funcion= (x=1,y=0) => {return x+y};
+const objeto = {nombre:'Variables en JSX'} // EN LOS OBJETOS REACT NO SABE COMO COVERTIRLOS
+const fecha = new Date();// EN LOS OBJETOS REACT NO SABE COMO COVERTIRLOS
+export const Componente = () => {
+    return(
+        <div>
+            <p>{texto}</p>
+            <p>{numeros}</p>
+            <p>{lista}</p>
+            <p>{funcion()}</p>
+            <p>{JSON.stringify(objeto)}</p> {/* Convierte el objeto en una cadena de texto formato JSON.*/}
+            <p>{JSON.stringify(fecha)}</p> {/* Convierte el objeto en una cadena de texto formato JSON.*/}   
+        </div>
+    ) // solo puede devolver un objeto, por eso se agrupa en el div
+}
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,17 +4,12 @@ import { StrictMode } from 'react'
 // Importa 'createRoot', el método moderno para decirle a React
 // dónde montar la aplicación en el DOM (el HTML real del navegador).
 import { createRoot } from 'react-dom/client'
-// Importa el archivo de estilos CSS global.
-// Se aplica a toda la aplicación.
-import './index.css'
-// Importa el componente principal de la aplicación, generalmente llamado 'App'.
-// Este componente actúa como el contenedor superior de toda tu interfaz de usuario.
-import App from './App.jsx'
 
+import { Componente } from './basico/componenteBasico'
 // Busca el elemento DOM donde se inyectará la aplicación.
 // En el archivo 'index.html' (típicamente), hay un <div> con id="root".
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Componente/>
   </StrictMode>,
 )

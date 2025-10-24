ca961ed135cde75887b825fb360bba23102fb4eb
Adrian Rubio <adrianrh13743@gmail.com>
2025-10-24 17:57:38 +0200

    v1.1-useEffect y Fetch

---
+++ client/src/basico/usuarios.jsx
@@ -0,0 +1,25 @@
+import {useState } from "react";
+import { ListaUsuarios } from "../components/componentes _usuarios/listaUsuarios";
+export const Usuarios = () =>{
+    // Inicialmente, se establece para pedir los datos de 'users'.
+    const [endPoint,setendPoint] =useState('users');
+    // Ejecuta la función fecthUsuarios después del renderizado inicial del componente.
+
+    // Función que se llama al hacer clic en el botón.
+    const handleFetch=()=>{
+        // Al cambiar el estado 'endPoint' a 'comments', React re-renderiza este componente.
+        // La nueva prop 'comments' se pasa al hijo (<ListaUsuarios>).
+        setendPoint('comments')
+    }
+    return(
+        <>
+            <h1>Contendido cargado {endPoint}:</h1>
+            <ListaUsuarios endPoint={endPoint}> </ListaUsuarios>
+            {/* 4. CONTROL: Botón que dispara el cambio de estado 'endPoint'. */}
+            <button onClick={handleFetch}>Llamado a la API</button>
+            <ul>
+            </ul>
+        </>
+    )
+}
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import { StrictMode } from 'react'
 // dónde montar la aplicación en el DOM (el HTML real del navegador).
 import { createRoot } from 'react-dom/client'
 
-import { Map } from './basico/map'
+import { Usuarios } from './basico/usuarios'
 
 import './styles.css';
 
@@ -13,6 +13,6 @@ import './styles.css';
 // En el archivo 'index.html' (típicamente), hay un <div> con id="root".
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Map  />
+    <Usuarios  />
   </StrictMode>,
 )
